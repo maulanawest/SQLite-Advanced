19433757e8e994cf1fbca63f9da0c0d85c51e15c
maulana2ahmad <43386555+maulana2ahmad@users.noreply.github.com>
2019-05-21 22:42:35 +0700

    Update MainActivity.java

--- app/src/main/java/com/redudant/sqliteadvanced/MainActivity.java
+++ app/src/main/java/com/redudant/sqliteadvanced/MainActivity.java
@@ -18,30 +18,47 @@ public class MainActivity extends AppCompatActivity {
             //Create Database "Users"
             SQLiteDatabase eventsDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
+            //new table using ID
+            eventsDB.execSQL("CREATE TABLE IF NOT EXISTS newUser (name, VARCHAR, age INTEGER(3), id INTEGER PRIMARY KEY)");
+
             //Create Table "users"
             //eventsDB.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
 
             //add value table
-            //eventsDB.execSQL("INSERT INTO users (name, age) VALUES ('Maulana', 25)");
+            //eventsDB.execSQL("INSERT INTO newUser (name, age) VALUES ('Maulana', 25)");
 
-            //eventsDB.execSQL("INSERT INTO users (name, age) VALUES ('Ahmad', 26)");
+            //eventsDB.execSQL("INSERT INTO newUser (name, age) VALUES ('Ahmad', 26)");
+
+            //delete using name and limit
+            //eventsDB.execSQL("DELETE FROM users WHERE name = 'Maulana' LIMIT 1");
+
+            //update
+            //eventsDB.execSQL("UPDATE users SET age = 100 WHERE name = 'Ahmad'");
+
+            //delete by name
+            //eventsDB.execSQL("DELETE FROM newUser WHERE name = 'Maulana'");
+
+            //delete by id
+            eventsDB.execSQL("DELETE FROM newUser WHERE id = 1");
 
             //Query
             //Cursor c = eventsDB.rawQuery("SELECT * FROM users", null);
 
             //Quesry dengan pengecualian dengan membatasi using WHERE
-            // use SELECT * FROM users WHERE name = 'Maulana'
-            Cursor c = eventsDB.rawQuery("SELECT * FROM users WHERE name = 'Maulana'", null);
+            // SELECT * FROM users WHERE name = 'Maulana'
+            // "SELECT * FROM users WHERE age > 21"
+            Cursor c = eventsDB.rawQuery("SELECT * FROM newUser", null);
 
             int nameIndext = c.getColumnIndex("name");
             int ageIndext = c.getColumnIndex("age");
+            int idIndext = c.getColumnIndex("id");
 
             c.moveToFirst();
 
-            while (c != null)
-            {
+            while (c != null) {
                 Log.i("UserResult - name", c.getString(nameIndext));
                 Log.i("UserResult - age", Integer.toString(c.getInt(ageIndext)));
+                Log.i("UserResult - id", Integer.toString(c.getInt(idIndext)));
 
                 c.moveToNext();
             }
